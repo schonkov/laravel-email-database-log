ac5fed05474443fe3a642cc0af8f4d4a01ae1d26
schonkov <mp5@mail.bg>
2024-08-28 16:28:21 +0300

    Update EmailLogController.php
    fixed broken attachments and body

--- src/EmailLogController.php
+++ src/EmailLogController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Dmcbrn\LaravelEmailDatabaseLog\Events\EventFactory;
+use Illuminate\Support\Facades\File;
 
 class EmailLogController extends Controller {
 
@@ -103,34 +104,75 @@ class EmailLogController extends Controller {
         $email = EmailLog::with('events')
             ->find($id);
 
+        // Ensure the body is correctly handled
+        if ($email && is_string($email->body)) {
+            // Decode the quoted-printable body if necessary
+            $email->body = quoted_printable_decode($email->body);
+        }
+
         //format attachments as collection
         $attachments = collect();
 
-        //check if there are any attachments
-        $attachmentsArray = array_filter(explode(', ',$email->attachments));
-        if(count($attachmentsArray) > 0) {
-            //set up new $email->attachments values
-            foreach($attachmentsArray as $key => $attachment) {
-                //update each attachment's value depending if file can be found on disk
-                $fileName = basename($attachment);
-                if(Storage::disk(config('email_log.disk'))->exists($attachment)) {
-                    $route = $isApi 
+        //was
+        // //check if there are any attachments
+        // $attachmentsArray = array_filter(explode(', ',$email->attachments));
+        // if(count($attachmentsArray) > 0) {
+        //     //set up new $email->attachments values
+        //     foreach($attachmentsArray as $key => $attachment) {
+        //         //update each attachment's value depending if file can be found on disk
+        //         $fileName = basename($attachment);
+        //         if(Storage::disk(config('email_log.disk'))->exists($attachment)) {
+        //             $route = $isApi 
+        //                 ? 'api.email-log.fetch-attachment'
+        //                 : 'email-log.fetch-attachment';
+        //             $formattedAttachment = [
+        //                 'name' => $fileName,
+        //                 'route' => route($route, [
+        //                     'id' => $email->id,
+        //                     'attachment' => $key,
+        //                 ]),
+        //             ];
+        //         } else {
+        //             $formattedAttachment = [
+        //                 'name' => $fileName,
+        //                 'message' => 'file not found',
+        //             ];
+        //         }
+        //         $attachments->push($formattedAttachment);
+        //     }
+        // }
+
+        // Handle attachments only if they exist
+        if (!empty($email->attachments)) {
+            $attachmentsArray = array_filter(explode(', ', $email->attachments));
+
+            // dd($attachmentsArray);
+            ///home/ubuntu/epodrome-multibranches/dev/storage/app/email_log_attachments/66cca1aa00f56/commande_details_1330_lundi 26 août 2024 15h39m.pdf
+            foreach ($attachmentsArray as $key => $attachment) {
+
+                $file_dir = storage_path('app/email_log_attachments/').$attachment;
+                // dd($file_dir, File::exists($file_dir), );
+                
+                
+                // Validate that the attachment is a valid path and exists on the disk
+                if ($this->isValidPath($attachment) && File::exists($file_dir)) {
+                    $route = $isApi
                         ? 'api.email-log.fetch-attachment'
                         : 'email-log.fetch-attachment';
-                    $formattedAttachment = [
-                        'name' => $fileName,
+
+                    $attachments->push([
+                        'name' => basename($attachment),
                         'route' => route($route, [
                             'id' => $email->id,
                             'attachment' => $key,
                         ]),
-                    ];
+                    ]);
                 } else {
-                    $formattedAttachment = [
-                        'name' => $fileName,
+                    $attachments->push([
+                        'name' => basename($attachment),
                         'message' => 'file not found',
-                    ];
+                    ]);
                 }
-                $attachments->push($formattedAttachment);
             }
         }
 
@@ -139,6 +181,12 @@ class EmailLogController extends Controller {
 
         //return
         return $email;
+    }
+
+    private function isValidPath(string $path): bool
+    {
+        // Implement logic to validate if the string is a valid path
+        return strpos($path, '/') !== false;
     }
 
     private function getEmailsForListing(Request $request)
